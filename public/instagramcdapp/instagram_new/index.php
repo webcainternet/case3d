5b1e25a0312bd06ad5532e4b6f98fcb1b43d1c4d
fernandofmendes <fernando.mendes@webca.com.br>
2015-09-24 06:55:59 -0300

    instagram URL

--- public/instagramcdapp/instagram_new/index.php
+++ public/instagramcdapp/instagram_new/index.php
@@ -13,7 +13,7 @@ use MetzWeb\Instagram\Instagram;
 $instagram = new Instagram(array(
 'apiKey'      => '8de0dedcca0f40618752cb632802949b',
 'apiSecret'   => 'a5b54552673047f793702a066d3e48fb',
-'apiCallback' => 'https://www.case3d.com.br/instagramcdapp/instagram/success.php' // must point to success.php
+'apiCallback' => 'https://case3d.com.br/instagramcdapp/instagram/success.php' // must point to success.php
 ));
 
 // create login URL
--- public/instagramcdapp/instagram_new/success.php
+++ public/instagramcdapp/instagram_new/success.php
@@ -6,7 +6,7 @@ use MetzWeb\Instagram\Instagram;
 $instagram = new Instagram(array(
 'apiKey'      => '8de0dedcca0f40618752cb632802949b',
 'apiSecret'   => 'a5b54552673047f793702a066d3e48fb',
-'apiCallback' => 'https://www.case3d.com.br/instagramcdapp/instagram/success.php' // must point to success.php
+'apiCallback' => 'https://case3d.com.br/instagramcdapp/instagram/success.php' // must point to success.php
 ));
 
 // Receive OAuth code parameter
