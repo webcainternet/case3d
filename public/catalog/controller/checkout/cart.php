f92b8301299d342ff09b323ba25afaed35e98c49
fernandofmendes <fernando.mendes@webca.com.br>
2016-02-05 03:08:37 -0200

    add 2 cart redirect 2 cart (COOL)

--- public/catalog/controller/checkout/cart.php
+++ public/catalog/controller/checkout/cart.php
@@ -611,6 +611,8 @@ class ControllerCheckoutCart extends Controller {
 
 				$json['success'] = sprintf($this->language->get('text_success'), $this->url->link('product/product', 'product_id=' . $this->request->post['product_id']), $product_info['name'], $this->url->link('checkout/cart'));
 				
+				$json['redirect'] = str_replace('&amp;', '&', $this->url->link('checkout/cart'));
+				
 				unset($this->session->data['shipping_method']);
 				unset($this->session->data['shipping_methods']);
 				unset($this->session->data['payment_method']);
